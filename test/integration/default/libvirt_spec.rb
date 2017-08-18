9ea02d6bf158fa34300ff0e2903a2cd708ebf318
bob5ec <bob5ec@users.noreply.github.com>
2017-08-18 15:19:33 +0200

    using DebOps to install libvirt

---
+++ test/integration/default/libvirt_spec.rb
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+# use basic tests
+describe package('libvirt-daemon') do
+  it { should be_installed }
+end
+
+# extend tests with metadata
+control '01' do
+  impact 0.7
+  title 'Verify libvirtd service'
+  desc 'Ensures libvirtd service is up and running'
+  describe service('libvirtd') do
+    it { should be_enabled }
+    it { should be_installed }
+    it { should be_running }
+  end
+end
+
+# implement os dependent tests
+libvirt_user = 'libvirt-qemu'
+
+describe user(libvirt_user) do
+  it { should exist }
+end
