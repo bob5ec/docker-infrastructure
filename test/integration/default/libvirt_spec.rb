1093dc3dadce2fdf08ccfdf44c9d3a3b23be7ebb
bob5ec <bob5ec@users.noreply.github.com>
2017-08-20 17:30:13 +0200

    test for libvirt via virsh

--- test/integration/default/libvirt_spec.rb
+++ test/integration/default/libvirt_spec.rb
@@ -6,20 +6,25 @@ describe package('libvirt-daemon') do
 end
 
 # extend tests with metadata
-control '01' do
-  impact 0.7
-  title 'Verify libvirtd service'
-  desc 'Ensures libvirtd service is up and running'
-  describe service('libvirtd') do
-    it { should be_enabled }
-    it { should be_installed }
-    it { should be_running }
-  end
-end
+#control '01' do
+#  impact 0.7
+#  title 'Verify libvirtd service'
+#  desc 'Ensures libvirtd service is up and running'
+#  describe service('libvirtd') do
+#    it { should be_enabled }
+#    it { should be_installed }
+#    it { should be_running }
+#  end
+#end
 
 # implement os dependent tests
 libvirt_user = 'libvirt-qemu'
 
 describe user(libvirt_user) do
   it { should exist }
+end
+
+title 'verify virsh can connect to libvirtd service'
+describe command('sudo virsh list') do
+  its('stdout') { should include('Id') }
 end
