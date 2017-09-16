6fb7804da538bea207a74b9607e45fdaa638d57e
bob5ec <bob5ec@users.noreply.github.com>
2017-09-16 14:52:58 +0200

    hardware preseed and deployment

--- deploy/deploy-ansible.sh
+++ deploy/deploy-ansible.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-apt-get install ansible
+#apt-get install ansible
+ansible-pull -U https://github.com/bob5ec/docker-infrastructure.git -C master -o default.yml
 ansible-pull -U https://github.com/bob5ec/docker-infrastructure.git -C master -o hardware.yml
