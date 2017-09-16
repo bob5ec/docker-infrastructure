db813ced60a98806d8f548369d7d0722236bd953
bob5ec <bob5ec@users.noreply.github.com>
2017-09-16 13:30:21 +0200

    preparing deplyment

--- deploy/deploy-ansible.sh
+++ deploy/deploy-ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt-get install ansible
-ansible-pull -U https://github.com/bob5ec/docker-infrastructure.git -C neo -o test/integration/default/default.yml
+ansible-pull -U https://github.com/bob5ec/docker-infrastructure.git -C master -o hardware.yml
