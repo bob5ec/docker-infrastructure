7b6d529c96cdadb82754ce7d620d93033228e5e2
Lars <bob5ec@users.noreply.github.com>
2017-10-25 22:45:29 +0200

    Update deploy-ansible.sh

--- deploy/deploy-ansible.sh
+++ deploy/deploy-ansible.sh
@@ -4,8 +4,8 @@ git clone https://github.com/bob5ec/docker-infrastructure.git
 ansible-galaxy install -r docker-infrastructure/requirements.yml
 echo localhost > hosts
 cd docker-infrastructure
-ansible-playbook default.yml --check -i ../hosts --connection=local
-ansible-playbook hardware.yml --check -i ../hosts --connection=local
+ansible-playbook default.yml -i ../hosts --connection=local
+ansible-playbook hardware.yml -i ../hosts --connection=local
 
 #old
 #ansible-pull -U https://github.com/bob5ec/docker-infrastructure.git -C master default.yml
