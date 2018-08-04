c834f5b1e28eba0181e1d9fefb2f95ba75d026bf
bob5ec <bob5ec@users.noreply.github.com>
2018-08-04 23:09:47 +0200

    create debian VMs call custom preseed TODO create ssh keys on metal and push them to VMs via virt-install.

---
+++ deploy/deploy-ansible-vm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#get public key
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+wget -O /root/.ssh/authorized_keys https://github.com/bob5ec.keys
+chmod 600 /root/.ssh/authorized_keys
+#ansible -i ~/admin/docker-infrastructure/deploy/ansible-hosts localhost -m authorized_key -a "user=lars state=present key=https://github.com/bob5ec.keys"
+
+#let metal do the rest
--- deploy/deploy-ansible.sh
+++ deploy/deploy-ansible.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-cd /tmp
+cd /etc
 git clone https://github.com/bob5ec/docker-infrastructure.git
+rm /etc/ansible/hosts
+ln -s /etc/docker-infrastructure/deploy/ansible-hosts /etc/ansible/hosts
 ansible-galaxy install -r docker-infrastructure/requirements.yml
-echo localhost > hosts
 cd docker-infrastructure
-ansible-playbook default.yml -i ../hosts --connection=local
-ansible-playbook hardware.yml -i ../hosts --connection=local
+ansible-playbook default.yml
+ansible-playbook hardware.yml
 
 #old
 #ansible-pull -U https://github.com/bob5ec/docker-infrastructure.git -C master default.yml
