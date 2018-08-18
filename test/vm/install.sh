277b9312d39630e77c5e8d183c3044d72371385d
bob5ec <bob5ec@users.noreply.github.com>
2018-08-19 01:25:55 +0200

    updating test to fit environments

--- test/vm/install.sh
+++ test/vm/install.sh
@@ -1,13 +1,12 @@
 virt-install \
     --connect qemu:///system \
-    --name "test2" \
+    --name "dev10" \
     --ram 1024 --vcpus 1 \
     --os-type=linux \
     --os-variant=virtio26 \
-    --disk "path=test2.qcow2,format=qcow2,size=8,bus=virtio" \
-    --graphics none \
+    --disk "path=dev10.qcow2,format=qcow2,size=8,bus=virtio" \
     --console pty,target_type=serial \
     --location 'http://mirror.netcologne.de/debian/dists/stretch/main/installer-amd64/' \
-    --extra-args 'console=ttyS0,115200n8 serial auto=true priority=critical netcfg/hostname=test2 preseed/url=https://raw.githubusercontent.com/bob5ec/docker-infrastructure/master/deploy/vm.preseed'
-    #--vnc --noautoconsole \
-    #--location 'ftp://ftp.de.debian.org/debian/dists/stretch/main/installer-amd64/' \
+    --extra-args 'console=ttyS0,115200n8 serial auto=true priority=critical netcfg/hostname=dev10 preseed/url=https://raw.githubusercontent.com/bob5ec/docker-infrastructure/dev/deploy/vm.preseed'
+# enable console for testing
+#--graphics none \
