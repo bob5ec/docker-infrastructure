ab29db46d92b609e39198fb3ef4292a2a4b3a9dd
bob5ec <bob5ec@users.noreply.github.com>
2018-08-18 23:11:27 +0200

    setting hostname in installer

--- test/vm/install.sh
+++ test/vm/install.sh
@@ -1,13 +1,13 @@
 virt-install \
     --connect qemu:///system \
-    --name "test1" \
+    --name "test2" \
     --ram 1024 --vcpus 1 \
     --os-type=linux \
     --os-variant=virtio26 \
-    --disk "path=test1.qcow2,format=qcow2,size=8,bus=virtio" \
+    --disk "path=test2.qcow2,format=qcow2,size=8,bus=virtio" \
     --graphics none \
     --console pty,target_type=serial \
-    --location 'http://ftp.de.debian.org/debian/dists/stretch/main/installer-amd64/' \
-    --extra-args 'console=ttyS0,115200n8 serial auto=true priority=critical preseed/url=https://raw.githubusercontent.com/bob5ec/docker-infrastructure/master/deploy/vm.preseed'
+    --location 'https://mirror.netcologne.de/debian/dists/stretch/main/installer-amd64/' \
+    --extra-args 'console=ttyS0,115200n8 serial auto=true priority=critical netcfg/hostname=test2 preseed/url=https://raw.githubusercontent.com/bob5ec/docker-infrastructure/master/deploy/vm.preseed'
     #--vnc --noautoconsole \
     #--location 'ftp://ftp.de.debian.org/debian/dists/stretch/main/installer-amd64/' \
