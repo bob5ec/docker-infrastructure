3146b419f437942ac9dac6e4f2648f22274def5f
bob5ec <bob5ec@users.noreply.github.com>
2018-11-17 13:04:06 +0100

    unified deployment of metal and vms

--- deploy/deploy-ansible.sh
+++ deploy/deploy-ansible.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
+
+env=$1
+ansible_group=$2
+# use git repository for initial config
 cd /etc
-#TODO set branch master when releaseing
-git clone https://github.com/bob5ec/docker-infrastructure.git
-rm /etc/ansible/hosts
-ln -s /etc/docker-infrastructure/deploy/ansible-hosts /etc/ansible/hosts
-ansible-galaxy install -r docker-infrastructure/requirements.yml
-cd docker-infrastructure
-ansible-playbook default.yml
-ansible-playbook hardware.yml
+git clone https://github.com/bob5ec/docker-infrastructure.git -b $env
+
+#rm /etc/ansible/hosts
+#ln -s /etc/docker-infrastructure/deploy/ansible-hosts-$2 /etc/ansible/hosts
+#ansible-galaxy install -r docker-infrastructure/requirements.yml
+#cd docker-infrastructure
+#ansible-playbook deploy.yml
+#ansible-playbook default.yml
+#ansible-playbook hardware.yml
+
+#configure ansible
+echo "[$ansible_group]" > /etc/ansible/hosts
+echo "localhost  ansible_connection=local" >> /etc/ansible/hosts
+#TODO move to deploy
+ansible-galaxy install -r /etc/docker-infrastructure/requirements.yml
+
+#run ansible-pull manually
+ansible-pull --extra-vars "env=$env, playbook=hardware" -U https://github.com/bob5ec/docker-infrastructure.git -C $env deploy.yml
 
 #old
 #ansible-pull -U https://github.com/bob5ec/docker-infrastructure.git -C master default.yml
