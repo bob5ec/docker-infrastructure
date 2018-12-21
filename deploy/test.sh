0fbdc36767a4a630abb876aa3310b1979a5b8d4c
bob5ec <bob5ec@users.noreply.github.com>
2018-12-21 12:27:17 +0100

    updating test deploy

--- deploy/test.sh
+++ deploy/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git pull
-echo localhost > /tmp/ansible_hosts
-ansible-playbook hardware.yml -i /tmp/ansible_hosts --connection=local
+#echo localhost > /tmp/ansible_hosts
+#ansible-playbook hardware.yml -i /tmp/ansible_hosts --connection=local
+env=dev
+ansible-pull --extra-vars "env=$env, playbook=hardware" -U https://github.com/bob5ec/docker-infrastructure.git -C $env deploy.yml
