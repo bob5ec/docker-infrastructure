2d3dfe85f5b03b9cf97b30b48539609242b19eef
bob5ec <bob5ec@users.noreply.github.com>
2019-01-05 15:25:22 +0100

    removing hardcoded env from deploy

--- deploy/test.sh
+++ deploy/test.sh
@@ -4,4 +4,4 @@ git pull
 #ansible-playbook hardware.yml -i /tmp/ansible_hosts --connection=local
 export ANSIBLE_STRATEGY=debug
 env=dev
-ansible-pull -v --extra-vars "deploy_env=$env deploy_playbook=hardware" -U https://github.com/bob5ec/docker-infrastructure.git -C $env hardware.yml
+ansible-pull -v --extra-vars "deploy_env=$env deploy_playbook=hardware.yml" -U https://github.com/bob5ec/docker-infrastructure.git -C $env hardware.yml
