03eea0a027c05d725f72a6bb936310581eb2df92
bob5ec <bob5ec@users.noreply.github.com>
2019-02-12 18:09:18 +0100

    minimize deploy playbook that runs during debian-installer

--- deploy/deploy-ansible.sh
+++ deploy/deploy-ansible.sh
@@ -5,12 +5,12 @@ ansible_group=$2
 # DEPRECATED: use git repository for initial config
 cd /etc
 git clone https://github.com/bob5ec/docker-infrastructure.git -b $env
-#TODO instll via ansible deploy playbook
+#TODO install via ansible deploy playbook
 ansible-galaxy install -r /etc/docker-infrastructure/requirements.yml
 
 #configure ansible
 echo "[$ansible_group]" > /etc/ansible/hosts
 echo "localhost  ansible_connection=local" >> /etc/ansible/hosts
 
-#run ansible-pull manually
+#run ansible-pull deploy playbook and set hardware playbook into cron
 ansible-pull --extra-vars "deploy_env=$env deploy_playbook=hardware.yml" -U https://github.com/bob5ec/docker-infrastructure.git -C $env deploy.yml
